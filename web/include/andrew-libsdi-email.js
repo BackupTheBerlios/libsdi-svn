5e9d165890186161f49aef43a2eddea385175591
apers <apers@7eb10eaa-2c00-0410-a9c0-ff75b171558f>
2005-09-08 00:02:19 +0000

    Initial web-stuff, mostly a copy from the spew project.

---
+++ web/include/andrew-libsdi-email.js
@@ -0,0 +1,68 @@
+<script type='text/javascript'>
+// <!-- 1350685073
+// This script is (C) Copyright 2004 Jim Tucek
+// Leave these comments alone!  For more info, visit
+// www.jracademy.com/~jtucek/email/ 
+
+function bend(algebra,sponge,seat) {
+algebra += ' ';
+var assistant = algebra.length;
+var bromide = 0;
+var horse = '';
+for(var song = 0; song < assistant; song++) {
+bromide = 0;
+while(algebra.charCodeAt(song) != 32) {
+bromide = bromide * 10;
+bromide = bromide + algebra.charCodeAt(song)-48;
+song++;
+}
+horse += String.fromCharCode(leap(bromide,sponge,seat));
+}
+parent.location = 'm'+'a'+'i'+'l'+'t'+'o'+':'+horse;
+}
+
+function build(crust,thing,day) {
+crust += ' ';
+var age = crust.length;
+var equipment = 0;
+for(var hope = 0; hope < age; hope++) {
+equipment = 0;
+while(crust.charCodeAt(hope) != 32) {
+equipment = equipment * 10;
+equipment = equipment + crust.charCodeAt(hope)-48;
+hope++;
+}
+//document.write('&');
+//document.write('#');
+//document.write(leap(equipment,thing,day));
+document.write(String.fromCharCode(leap(equipment,thing,day)));
+}
+}
+
+function leap(history,information,insect) {
+if (insect % 2 == 0) {
+side = 1;
+for(var mortality = 1; mortality <= insect/2; mortality++) {
+mission = (history*history) % information;
+side = (mission*side) % information;
+}
+} else {
+side = history;
+for(var reason = 1; reason <= insect/2; reason++) {
+mission = (history*history) % information;
+side = (mission*side) % information;
+}
+}
+return side;
+}
+// -->
+</script>
+
+<!-- Clicking on this link uses javascript to decrypt the email address -->
+<a href='javascript:bend("2424 3344 4258 3018 4385 444 603 2462 955 769 2117 4258 955 636 89 444 4385 1785 4731 4772 2576",5029,3251)'>Click to email me</a>
+
+<BR>- or -<BR>
+
+<!-- Alternativly, you can have the script display the text of your email address -->
+<!-- which is less secure, but it's better than nothing and looks better -->
+Email me at <a href='javascript:bend("2424 3344 4258 3018 4385 444 603 2462 955 769 2117 4258 955 636 89 444 4385 1785 4731 4772 2576",5029,3251)' onMouseOver="self.status=''; return true;" onMouseOut="self.status=' '; return true;"><script type='text/javascript'>build("2424 3344 4258 3018 4385 444 603 2462 955 769 2117 4258 955 636 89 444 4385 1785 4731 4772 2576",5029,3251)</script></a>  (A tad bit less secure, but looks better.  So, the question is whether you're shallow or not. ;)
